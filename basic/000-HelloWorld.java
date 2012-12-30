da632d48e7bcf7e628742308157811a5f5aaac1b
Adam Carmichael <carneeki@carneeki.net>
2012-12-31 00:24:56 +1100

    Add the HelloWorld example

---
+++ basic/000-HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * the classic Hello World! example.
+ *
+ * Everything in Java is in a class. A class is a way to think of an object. For
+ * now, pretend the next couple of lines are how every program starts. We will
+ * cover how they work in a couple of lessons I promise!
+ *
+ */
+class HelloWorld
+{
+  public static void main( String[] args )
+  {
+    System.out.println("Hello World!"); // System.out.println() is what we use
+                                        // to everything on a new line.
+                                        // It is "Print Line". Very handy for
+                                        // debugging!
+  }
+}
